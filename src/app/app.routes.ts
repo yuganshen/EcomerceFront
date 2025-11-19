b930c8a2f4478b54968e4e7c2528a31ad98e6839
ShadowIX-9 <ramosenderplayyt@gmail.com>
2025-11-18 23:52:11 -0500

    Gestion de productos agregado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { InicioComponent } from './paginas/inicio/inicio.component';
 import {  MemoriasRamComponent } from './paginas/memorias-ram/memorias-ram.component';
 import { ProcesadoresComponent } from './paginas/procesadores/procesadores.component';
 import { CarritoComponent } from './paginas/carrito/carrito.component';
+import { GestionProductosComponent } from './paginas/gestionproductos/gestionproductos.component';
 
 export const routes: Routes = [
      { path: '', component: InicioComponent }, // página principal
    { path: 'carrito', component: CarritoComponent },
       { path: 'memorias', component: MemoriasRamComponent },
-      { path: 'procesadores', component: ProcesadoresComponent }
+      { path: 'procesadores', component: ProcesadoresComponent },
+      { path: 'gestionproductos', component: GestionProductosComponent }
 
 ];
