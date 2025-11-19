b97d4329632588b6e2541fa1c89ec578d5c92787
Luis Fernando <luchohuilcahusca@gmail.com>
2025-11-19 00:07:23 -0500

    ohhh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { InicioComponent } from './paginas/inicio/inicio.component';
 import {  MemoriasRamComponent } from './paginas/memorias-ram/memorias-ram.component';
 import { ProcesadoresComponent } from './paginas/procesadores/procesadores.component';
 import { CarritoComponent } from './paginas/carrito/carrito.component';
+import { LoginComponent } from './paginas/login/login.component';
 
 export const routes: Routes = [
      { path: '', component: InicioComponent }, // página principal
-   { path: 'carrito', component: CarritoComponent },
+    { path: 'carrito', component: CarritoComponent },
       { path: 'memorias', component: MemoriasRamComponent },
-      { path: 'procesadores', component: ProcesadoresComponent }
-
+      { path: 'procesadores', component: ProcesadoresComponent },
+      { path: 'login',component:LoginComponent}
 ];
